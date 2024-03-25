cc565517f8adfe636c30f1818e5ca546ec510105
yeongtaek <144389087+yeonggtaek@users.noreply.github.com>
2024-03-25 11:09:48 -0700

    button

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Button from "@/Components/Button";
+import styles from "./about.module.css";
 
 export default function about() {
   return (
+    <section className={styles.about}>
     <div>
       <Button link="/quiz">Next</Button>
     </div>
+    </section>
   );
 }
